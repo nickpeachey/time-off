5b3d2456a2c949586f909c97b716daf51d9c0b29
Nick Peachey <nick.peachey@gmail.com>
2015-11-30 16:45:17 +0000

    initial

---
+++ spec/simple_math_spec.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+
+describe 'simple specs with math' do
+
+	it 'should add correctly' do
+		numberone = 3
+		numebertwo = 3
+		result = numberone + numebertwo
+		expect(result).to eq(3)
+	end
+
+		it 'should add correctly plus 1' do
+		numberone = 3
+		numebertwo = 3
+		result = numberone + numebertwo
+		expect(result).to eq(3)
+	end
+
+		it 'adding one to one' do
+		numberone = 3
+		numebertwo = 3
+		result = numberone + numebertwo
+		expect(result).to eq(3)
+	end
+
+		it 'should add correctly plus 3' do
+		numberone = 3
+		numebertwo = 3
+		result = numberone + numebertwo
+		expect(result).to eq(3)
+	end
+
+		it 'should add correctly plus 4' do
+		numberone = 3
+		numebertwo = 3
+		result = numberone + numebertwo
+		expect(result).to eq(6)
+	end
+
+end
